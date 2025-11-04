c6a9a0d54b94a194f4291da58bf4e54effdc9bd9
unknown <matan.cutler@runi.post.ac.il>
2025-11-05 00:29:15 +0200

    ex 1-3

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,18 @@
-//feedback
 
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+         String word = args[0];
+         int N = Integer.parseInt(args[1]);
+         String vowels = "aeiouyAEIOUY";
+
+         for (int i = 0 ; i < word.length() ; i ++){
+                if (vowels.indexOf(word.charAt(i)) != -1)
+                  System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                else
+                 System.out.println("Give me a " + word.charAt(i) + ": " + word.charAt(i) + "!");
+         } 
+         for (int i = 0; i<=N ; i++){
+                System.out.println(word + "!!!");
+         }
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,24 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String timeString = args[0];
+		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int tta = Integer.parseInt(args [1]);
+        int totalM = hours * 60 + minutes + tta; 
+        int totlH = totalM / 60;
+        int newH = totlH % 24;
+        int newM = totalM - (totlH * 60);
+        String NewM = "";
+
+        if(newM<10){
+            NewM = "0" + newM;
+        }
+        if(newH<10){
+            System.out.println("0" + newH + ": " + NewM);
+        }
+        else{
+            System.out.println(newH + ":" + NewM);
+        }
+       
     }
 }
