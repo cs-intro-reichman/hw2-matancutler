6ce5b5df0d979aefb66c34aa4ce31910ea0a0b8e
unknown <matan.cutler@runi.post.ac.il>
2025-11-08 15:45:59 +0200

    done

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,6 +1,5 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        String timeString = args[0];
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         int tta = Integer.parseInt(args [1]);
