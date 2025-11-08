65eae692f0d9431911b3a0c107bd10bbdb67a9de
unknown <matan.cutler@runi.post.ac.il>
2025-11-08 12:26:47 +0200

    done

--- Cheers.java
+++ Cheers.java
@@ -1,15 +1,15 @@
 
 public class Cheers {
         public static void main(String[] args) {
-         String word = args[0];
+         String word = args[0].toUpperCase();
          int N = Integer.parseInt(args[1]);
-         String vowels = "aAeEfFhHiIlLmMnNoOrRsSxX";
+         String vowels = "AEFHILMNORSX";
 
          for (int i = 0 ; i < word.length() ; i ++){
                 if (vowels.indexOf(word.charAt(i)) != -1)
                   System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
                 else
-                 System.out.println("Give me a " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                 System.out.println("Give me a  " + word.charAt(i) + ": " + word.charAt(i) + "!");
          } 
          System.out.println(("What does that spell?"));
          for (int i = 1; i<=N ; i++){
