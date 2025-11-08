2fe9a5a1c6fa3cad84647dff92999fc0dc578cf9
unknown <matan.cutler@runi.post.ac.il>
2025-11-08 12:10:32 +0200

    done

--- Cheers.java
+++ Cheers.java
@@ -7,9 +7,9 @@ public class Cheers {
 
          for (int i = 0 ; i < word.length() ; i ++){
                 if (vowels.indexOf(word.charAt(i)) != -1)
-                  System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                  System.out.println("Give me a " + word.charAt(i) + ": " + word.charAt(i) + "!");
                 else
-                 System.out.println("Give me a " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                 System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
          } 
          for (int i = 0; i<N ; i++){
                 System.out.println(word + "!!!");
